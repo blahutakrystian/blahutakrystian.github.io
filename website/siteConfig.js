ab33bd3aa33f7a719c3a6b344bdf34626e4d89e2
blahutakrystian <blahutakrystian@gmail.com>
2020-01-21 21:55:41 +0100

    githubpages commit

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -23,15 +23,15 @@
 const siteConfig = {
   title: 'SimpleLogic', // Title for your website.
   tagline: 'Logistic with us is easy!',
-  url: 'https://your-docusaurus-test-site.com', // Your website URL
-  baseUrl: '/', // Base URL for your project */
+  url: 'https://blahutakrystian.github.io', // Your website URL
+  baseUrl: '/simple_logic_documentation/', // Base URL for your project */
   // For github.io type URLs, you would set the url and baseUrl like:
   //   url: 'https://facebook.github.io',
   //   baseUrl: '/test-site/',
 
   // Used for publishing and more
-  projectName: 'SimpleLogic',
-  organizationName: 'B&K',
+  projectName: 'simple_logic_documentation',
+  organizationName: 'blahutakrystian',
   // For top-level user or org sites, the organization is still the same.
   // e.g., for the https://JoelMarcey.github.io site, it would be set like...
   //   organizationName: 'JoelMarcey'
